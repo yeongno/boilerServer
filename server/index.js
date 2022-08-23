44fd5aa5e6194591494e9d89e6897ccb42adcc18
yeongno <dldudsh83@gmail.com>
2022-08-23 17:42:18 +0900

    12

--- server/index.js
+++ server/index.js
@@ -38,12 +38,13 @@ app.use("/uploads", express.static("uploads"));
 // Serve static assets if in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("client/build"));
+  // app.use(express.static("client/build"));
   // app.use(express.static("/build"));
+  app.use(express.static("public"));
 
   // index.html for all page routes
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
